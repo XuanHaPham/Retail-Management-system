371007ee5635bd5b7346cb22beb696d9f5ac89b6
XuanHaPham <43726863+XuanHaPham@users.noreply.github.com>
2019-09-04 21:54:01 +0700

    Init API

---
+++ RetailManagementSystem/src/main/java/fpt/edu/RetailManagementSystem/RetailManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package fpt.edu.RetailManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailManagementSystemApplication.class, args);
+	}
+
+}
---
+++ RetailManagementSystem/src/test/java/fpt/edu/RetailManagementSystem/RetailManagementSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package fpt.edu.RetailManagementSystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RetailManagementSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
